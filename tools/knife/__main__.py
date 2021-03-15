1756ea59ba47bdaf741e0a3a689ebed29c386108
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-03-15 15:05:13 +0300

    remove group and all commands from __main__.py

--- tools/knife/__main__.py
+++ tools/knife/__main__.py
@@ -1,38 +1,4 @@
-import click
-from knife.config import Config
+from knife.group import cli_group
 
-@click.group()
-def cli():
-    pass
-
-@cli.command()
-@click.option(
-    "--config",
-    "config_path",
-    type=click.Path(resolve_path=True, exists=True)
-)
-@click.option("--override", type=str)
-@click.argument(
-    "template",
-    type=click.Path(
-        resolve_path=True,
-        exists=True,
-        dir_okay=False,
-    )
-)
-def render(config_path, template, override=None):
-    config = Config.from_path(config_path)
-    if override:
-        config.set_override(override)
-    config.render(template)
-
-@cli.command()
-def lock():
-    pass
-
-@cli.command()
-def unlock():
-    pass
-
-if __name__ == "__main__":
-    cli()
+def main():
+    cli_group()
