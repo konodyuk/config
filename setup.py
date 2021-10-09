e16c26cd2e8abf3e562ac602c2303649831f6fcc
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-10-09 23:24:58 +0300

    update requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,14 @@ setuptools.setup(
     author_email="konodyuk@gmail.com",
     description="Monotool",
     packages=setuptools.find_packages(include="mt*"),
-    install_requires=[],
+    install_requires=[
+        "attrs",
+        "click",
+        "jinja2",
+        "omegaconf",
+        "pyinfra",
+        "watchgod",
+    ],
     entry_points={
         "console_scripts": [
             "mt=mt.cli.main:main",
