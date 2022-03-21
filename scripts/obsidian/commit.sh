89ecc32c7ad0d88905fbac1a46c67b49ec59b48a
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2022-03-21 17:49:52 +0400

    add comments

--- scripts/obsidian/commit.sh
+++ scripts/obsidian/commit.sh
@@ -6,12 +6,16 @@ LOGDIR=$HOME/config/scripts/obsidian/logs
 exec >>$LOGDIR/stdout.log
 exec 2>>$LOGDIR/stderr.log
 
+# enter vault
 cd $HOME/Obsidian/main
 
+# get commit message prefix from timestamp
 PREFIX="CRON ($(date +"%Y-%m-%d %H:%M:%S"))"
 
+# log sync start
 echo "SYNC $PREFIX"
 
+# content repo: reset index and commit files
 git reset
 git add -A js && git commit -m "$PREFIX: js"
 git add -A typed && git commit -m "$PREFIX: notes"
@@ -19,6 +23,7 @@ git add -A index INDEX.md && git commit -m "$PREFIX: indices"
 git add typing.otl && git commit -m "$PREFIX: schema"
 git add -A files && git commit -m "$PREFIX: files"
 
+# config submodule: reset index and commit files
 cd .obsidian
 git reset
 git add -A plugins && git commit -m "$PREFIX: update plugins"
@@ -27,4 +32,5 @@ git add *.json && git commit -m "$PREFIX: update settings"
 git push
 cd ..
 
+# content repo: update submodule rev if necessary
 git add .obsidian && git commit -m "$PREFIX: update config submodule revision"
