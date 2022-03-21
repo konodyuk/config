0c2293b756c9cc70998b7b68aedc699a34992253
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2022-03-21 13:01:21 +0400

    add obsidian backup commit script

---
+++ scripts/obsidian/commit.sh
@@ -0,0 +1,20 @@
+cd $HOME/Obsidian/main
+
+PREFIX="CRON ($(date +"%Y-%m-%d %H:%M:%S"))"
+
+echo "SYNC $PREFIX"
+
+git reset
+git add -A js && git commit -m "$PREFIX: js"
+git add -A typed && git commit -m "$PREFIX: notes"
+git add -A index INDEX.md && git commit -m "$PREFIX: indices"
+git add typing.otl && git commit -m "$PREFIX: schema"
+git add -A files && git commit -m "$PREFIX: files"
+
+cd .obsidian
+git reset
+git add -A plugins && git commit -m "$PREFIX: update plugins"
+git push
+cd ..
+
+git add .obsidian && git commit -m "$PREFIX: update config submodule revision"
