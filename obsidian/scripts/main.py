09015193b9a96c6c1b9513cb4c7ce1a628fb6e42
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2022-10-01 15:06:25 +0400

    remove legacy stuff

--- obsidian/scripts/main.py
+++
@@ -1,9 +0,0 @@
-from handler import handler
-
-import datetime_handlers
-import markdown_handlers
-import svg_handlers
-import sympy_handlers 
-
-if __name__ == "__main__":
-    handler.run()
--- obsidian/scripts/svg_handlers.py
+++
@@ -1,5 +0,0 @@
-from handler import handler
-
-@handler.command(prefix="svg:", cut_prefix=True)
-def svg_handler(text, context):
-    pass
