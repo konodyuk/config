93fed950cda00858ee071aa81eec19bf53274e69
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2022-10-16 14:39:18 +0400

    add macos_defaults deploy

---
+++ deploys/macos_defaults.py
@@ -0,0 +1,44 @@
+from pyinfra.operations import server
+
+# ref: https://github.com/mathiasbynens/dotfiles/blob/66ba9b3cc0ca1b29f04b8e39f84e5b034fdb24b6/.macos#L148
+server.shell(
+    [
+        "defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true",
+        "defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144",
+    ],
+    name="Enable zoom with Ctrl+Scroll",
+)
+
+# ref: https://github.com/mathiasbynens/dotfiles/blob/66ba9b3cc0ca1b29f04b8e39f84e5b034fdb24b6/.macos#L157
+server.shell(
+    [
+        "defaults write NSGlobalDomain KeyRepeat -int 1",
+        "defaults write NSGlobalDomain InitialKeyRepeat -int 10",
+    ],
+    name="Set fast key repetition",
+)
+
+# ref: https://github.com/mathiasbynens/dotfiles/blob/66ba9b3cc0ca1b29f04b8e39f84e5b034fdb24b6/.macos#L436
+server.shell(
+    [
+        "defaults write com.apple.dock wvous-br-corner -int 10",
+        "defaults write com.apple.dock wvous-br-modifier -int 0",
+    ],
+    name="Hot corner: bottom right -> display sleep",
+)
+
+# ref: https://github.com/mathiasbynens/dotfiles/blob/66ba9b3cc0ca1b29f04b8e39f84e5b034fdb24b6/.macos#L191
+server.shell(
+    [
+        "pmset -a displaysleep 15",
+    ],
+    name="Display: sleep after 15 minutes",
+    _sudo=True,
+)
+
+server.shell(
+    [
+        "defaults write NSGlobalDomain com.apple.sound.uiaudio.enabled -int 0",
+    ],
+    name="Disable system beeps",
+)
