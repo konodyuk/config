5fa6a22f631bebb57c7e855b7d5a386b475e5458
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2023-01-29 21:08:05 +0400

    add sql interpreter to iron

--- dotfiles/nvim/lua/conf/iron.lua
+++ dotfiles/nvim/lua/conf/iron.lua
@@ -19,9 +19,9 @@ iron.setup({
 			applescript = {
 				command = { "osascript", "-i" },
 			},
-            sql = {
-                command = { "sqlite3" },
-            }
+			sql = {
+				command = { "sqlite3" },
+			},
 		},
 		-- how the REPL window will be opened, the default is opening
 		-- a float window of height 40 at the bottom.
