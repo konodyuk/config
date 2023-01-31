9ab154eebb26a36fdb759a8937d660448ca525b1
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2023-01-31 15:41:39 +0400

    configure gopls

--- dotfiles/nvim/lua/conf/lsp/lsp-installer.lua
+++ dotfiles/nvim/lua/conf/lsp/lsp-installer.lua
@@ -21,6 +21,28 @@ lsp_installer.on_server_ready(function(server)
 		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	end
 
+	if server.name == "gopls" then
+		-- ref: https://github.com/neovim/nvim-lspconfig/issues/804#issuecomment-1274928702
+		local gopls_opts = {
+			root_dir = function(fname)
+				local nvim_lsp = require("lspconfig")
+				local lastRootPath = nil
+				local gomodpath = vim.trim(vim.fn.system("go env GOPATH")) .. "/pkg/mod"
+				local fullpath = vim.fn.expand(fname, ":p")
+				if string.find(fullpath, gomodpath) and lastRootPath ~= nil then
+					return lastRootPath
+				end
+				local root = nvim_lsp.util.root_pattern("go.mod", ".git")(fname)
+				if root ~= nil then
+					lastRootPath = root
+				end
+				-- fix: root -> lastRootPath
+				return lastRootPath
+			end,
+		}
+		opts = vim.tbl_deep_extend("force", gopls_opts, opts)
+	end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
